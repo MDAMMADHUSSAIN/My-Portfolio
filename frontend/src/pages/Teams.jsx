ad3ef042f0312048594b37c37b0e5d7285cafe15
MD AMMAD HUSSAIN <md.ammad.hussain@gmail.com>
2025-12-18 01:12:29 +0530

    updated teams page - coming soon

--- frontend/src/pages/Teams.jsx
+++ frontend/src/pages/Teams.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
+
+
 
 const Teams = () => {
-  return (
-    <div>Teams</div>
-  )
-}
 
-export default Teams
+
+
+  return (
+    <div className="w-full min-h-screen bg-gradient-to-br from-[#020617] via-[#0f172a] to-black px-4 py-16 text-white">
+      {/* Header */}
+      <div className="text-center mb-16">
+        <motion.h1
+          initial={{ y: -50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.8 }}
+          className="text-4xl md:text-5xl font-bold text-blue-400"
+        >
+          This Page will be Coming soon !
+        </motion.h1>
+        <motion.p
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.8, delay: 0.3 }}
+          className="text-gray-100 mt-4 max-w-2xl mx-auto"
+        >
+          Thanks for your Patience
+        </motion.p>
+      </div>
+    </div>
+  );
+};
+
+export default Teams;
