4b5768afab6578a62bfa04d525b6e088c4d5ab0e
MD AMMAD HUSSAIN <md.ammad.hussain@gmail.com>
2025-12-17 20:51:29 +0530

    updated card component

--- frontend/src/components/TeamCard.jsx
+++ frontend/src/components/TeamCard.jsx
@@ -5,16 +5,7 @@ import Profile from "../assets/profile.jpg";
 const TeamCard = () => {
     const items = [
   
-  {
-    image: Profile,
-    title: "Abu Talib H.",
-    subtitle: "Full Stack Developer | @Somewhere",
-    handle: "@abutalibhussain",
-    borderColor: "#10B981",
-    gradient: "linear-gradient(145deg, #3B82F6, #000)",
-    url: "https://linkedin.com/in/"
-  },
-  {
+ {
     image: Profile,
     title: "Md Ammad H.",
     subtitle: "Full Stack Developer | @Somewhere",
@@ -23,15 +14,6 @@ const TeamCard = () => {
     gradient: "linear-gradient(180deg, #10B981, #000)",
     url: "https://github.com/mdammadhussain21"
   },
-  {
-    image: Profile,
-    title: "Mohd Sameer",
-    subtitle: "Full Stack Developer | @Somewhere",
-    handle: "@mohdsameer",
-    borderColor: "#3B82F6",
-    gradient: "linear-gradient(145deg, #3B82F6, #000)",
-    url: "https://linkedin.com/in/"
-  }
 ];
   return (
 
