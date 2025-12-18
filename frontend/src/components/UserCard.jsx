a7d98308bd0d850eb8fe43b22011ee361fbd9dc5
MD AMMAD HUSSAIN <md.ammad.hussain@gmail.com>
2025-12-19 04:18:58 +0530

    minor update

--- frontend/src/components/UserCard.jsx
+++ frontend/src/components/UserCard.jsx
@@ -8,11 +8,11 @@ const TeamCard = () => {
  {
     image: Profile,
     title: "Md Ammad H.",
-    subtitle: "Full Stack Developer | @Somewhere",
+    subtitle: "Full Stack Developer | @Kuwait",
     handle: "@ammadhussain2109",
     borderColor: "#3B82F6",
     gradient: "linear-gradient(180deg, #10B981, #000)",
-    url: "https://github.com/mdammadhussain21"
+    url: "https://github.com/MDAMMADHUSSAIN"
   },
 ];
   return (
